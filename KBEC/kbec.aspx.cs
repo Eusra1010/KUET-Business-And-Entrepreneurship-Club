49a52f3c43b4f60d4ab2759ba9e44e35c3159d3a
Eusra <eusra2207076@stud.kuet.ac.bd>
2026-06-12 00:36:59 +0600

    chore: fix gitignore and re-add all tracked files

---
+++ KBEC/kbec.aspx.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KBEC
+{
+    public partial class kbec : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
